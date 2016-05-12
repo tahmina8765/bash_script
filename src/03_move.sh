4a3b6e6ddeb31fa7c857bda4cff731615b591a04
root <root@dv1.shoplandia.co>
2016-05-12 07:36:53 -0500

    add more data in config

--- src/03_move.sh
+++ src/03_move.sh
@@ -4,3 +4,16 @@
 # Step-3: Move all /tmp files to /inbound through sftp
 # Author: Tahmina Khatoon, 2016
 ################################################################################
+
+#Configuration
+source "/var/www/bash_script/src/config"
+
+#SFTP file upload
+sftp root@104.251.216.38 << REMOTE
+
+cd $DIS_DIR
+put -r ${SRC_DIR}/*csv ${DIS_DIR}/
+
+quit
+
+REMOTE
