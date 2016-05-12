dbaec54e239700942e271faa2fd80cabfec12ade
root <root@dv1.shoplandia.co>
2016-05-12 08:57:48 -0500

    Task 3 done with ssh

--- src/03_move.sh
+++ src/03_move.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 ################################################################################
 # Script for Pitney Bowes: Upload a file to a target folder ETC
-# Step-3: Move all /tmp files to /inbound through sftp
+# Step-3: Move files from /tmp folder to /inblund folder with ssh
 # Author: Tahmina Khatoon, 2016
 ################################################################################
 
 #Configuration
 source "/var/www/bash_script/src/config"
 
-#SFTP file upload
-sftp root@104.251.216.38 << REMOTE
 
-cd $DIS_DIR
-put -r ${SRC_DIR}/*csv ${DIS_DIR}/
+#SSH file move
+ssh ${REMOTE_USER}@${REMOTE_HOST} "mv ${DIS_DIR}/* ${INBOUND_DIR}/."
 
-quit
-
-REMOTE
