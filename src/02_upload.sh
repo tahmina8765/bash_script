aa8f56e36fe8ee46ef0d6104585da541f7548531
root <root@dv1.shoplandia.co>
2016-05-12 07:20:16 -0500

    remove unnecessary files

--- src/02_upload.sh
+++ src/02_upload.sh
@@ -8,22 +8,11 @@
 #Configuration
 source "/var/www/bash_script/src/config"
 
-#FILES=$(ls ${SRC_DIR}/*csv)
-
-#for FILE in $FILES
-#do
-#    echo "${FILE}"
-#done
-
-#cd $SRC_DIR
 #SFTP file upload
 sftp root@104.251.216.38 << REMOTE
 
 cd $DIS_DIR
 put -r ${SRC_DIR}/*csv ${DIS_DIR}/
-
-#put $LOG_FILE
-
 
 quit
 
